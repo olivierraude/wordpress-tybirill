e474eff6ddeb6e06d53c8e2b1759a4fa6cfd18a2
olivierraude <olivierraude@gmail.com>
2023-11-14 15:18:43 -0500

    Modif nav

--- web/app/themes/themetybirill/header.php
+++ web/app/themes/themetybirill/header.php
@@ -22,7 +22,7 @@
       ?>
 
       <ul>
-        <li><a href="#">Certificat cadeau -> The ultimate</a></li>
+        <li><a href="#">Certificat cadeau</a></li>
       </ul>
     </div>
     <div class="logo-nav"><img src="./app/uploads/2023/10/logo-nav.png" alt="Pizzeria - Ferme Ty Birill"></div>
